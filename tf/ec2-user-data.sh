356d1f38790e1f93ebaedf08b5de2ea0141f588a
Jack <jackmurphy756@gmail.com>
2023-10-22 00:52:32 +0100

    Fix: update user-data script to download app

--- tf/ec2-user-data.sh
+++ tf/ec2-user-data.sh
@@ -8,13 +8,12 @@ yum install unzip
 systemctl start httpd
 systemctl enable httpd
 
-BUCKET=arn:aws:s3:::infra-1
+BUCKET=infra-1
 PATH_TO_KEY='app/dist_dir.zip'
 OUT_FILE=dist_dir.zip
 
-/usr/local/bin/aws s3api get-object --bucket $BUCKET --key $PATH_TO_KEY $OUT_FILE
+/usr/bin/aws s3api get-object --bucket $BUCKET --key $PATH_TO_KEY $OUT_FILE
 
-unzip $OUT_FILE -l -o -d /var/www/html/
+unzip $OUT_FILE
 
-# echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
-# cp --verbose --update -r -t /var/www/html $/*
+cp --verbose --update -r -t /var/www/html dist/*
