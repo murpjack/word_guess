1053a2f4b0f224c0f1591f6601d04b32b7e9b05c
Jack <jackmurphy756@gmail.com>
2023-10-21 23:55:03 +0100

    Feat: download object from s3 in user-data script

--- tf/ec2-user-data.sh
+++ tf/ec2-user-data.sh
@@ -3,8 +3,18 @@
 # install httpd (Linux 2 version)
 yum update -y
 yum install -y httpd
+yum install unzip
+
 systemctl start httpd
 systemctl enable httpd
 
+BUCKET=arn:aws:s3:::infra-1
+PATH_TO_KEY='app/dist_dir.zip'
+OUT_FILE=dist_dir.zip
+
+/usr/local/bin/aws s3api get-object --bucket $BUCKET --key $PATH_TO_KEY $OUT_FILE
+
+unzip $OUT_FILE -l -o -d /var/www/html/
+
 # echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
-cp --verbose --update -r -t /var/www/html ../dist/*
+# cp --verbose --update -r -t /var/www/html $/*
