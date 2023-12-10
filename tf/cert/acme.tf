e44a61ac6b08405e3557f00a5f7f241b2e4ce4a4
Jack <jackmurphy756@gmail.com>
2023-12-10 19:57:23 +0000

    fix: point to production cert

--- tf/cert/acme.tf
+++ tf/cert/acme.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "acme" {
-  server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
+  server_url = "https://acme-v02.api.letsencrypt.org/directory"
 }
 
 resource "tls_private_key" "private_key" {
