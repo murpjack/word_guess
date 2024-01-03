ecfc7beba793fef22f89cfbc89838438bf3a41f9
Jack <jackmurphy756@gmail.com>
2024-01-03 21:28:42 +0000

    Fix: update files included in release source

---
+++ utils/make_release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+echo "Retrieving latest tag."
+tag_name=$(git describe --tags)
+
+echo "Retrieving possible release for tag $tag_name."
+if [ $(gh release view "$tag_name") ]; then
+  echo "No release created."
+
+else
+  echo "No release currently exists for tag $tag_name. Attempting to create..."
+
+  # TODO: Add a pre-commit hook to enforce tagging of commits
+  # TODO: Add a pre-commit hook to enforce a changelog
+  gh release create $tag_name --title "$tag_name" $@
+fi
